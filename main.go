3b32a7fef3a8af92551e723f4fc6eac15f6ff6e0
oglinuk <daniel-l-black@protonmail.com>
2021-12-23 15:31:07 -0800

    Add simple REPL

--- main.go
+++ main.go
@@ -1,11 +1,50 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"log"
+	"math/rand"
 	"net"
 	"os"
+	"strings"
+	"time"
 )
+
+func handle(c net.Conn) {
+	defer c.Close()
+
+	br := bufio.NewReader(c)
+
+	remoteAddr := c.RemoteAddr().String()
+
+	for {
+		fmt.Fprintf(c, "> ")
+
+		line, err := br.ReadString('\n')
+		if err != nil {
+			if err == io.EOF {
+				continue
+			}
+			log.Println(err)
+		}
+
+		line = strings.TrimSpace(line)
+		log.Printf("%s > %s\n", remoteAddr, line)
+
+		switch line {
+			case "ip":
+				fmt.Fprintf(c, "%s\n", remoteAddr)
+			case "rng":
+				fmt.Fprintf(c, "%d\n", rand.Int63())
+			case "time":
+				fmt.Fprintf(c, "%s\n", time.Now())
+			default:
+				fmt.Fprintf(c, "commands: ip|rng|time\n")
+		}
+	}
+}
 
 func main() {
 	PORT := os.Getenv("PORT")
@@ -24,10 +63,12 @@ func main() {
 	for {
 		conn, err := lis.Accept()
 		if err != nil {
-			log.Fatal(err)
+			log.Println(err)
 		}
 
 		welcome := fmt.Sprintf("Hello %s!\n", conn.RemoteAddr().String())
 		conn.Write([]byte(welcome))
+
+		go handle(conn)
 	}
 }
